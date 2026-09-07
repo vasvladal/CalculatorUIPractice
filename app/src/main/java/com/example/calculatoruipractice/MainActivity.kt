d346631ade37e2072f15b1fa66e080556dbd9e92
vasvladal <vasvladal@users.noreply.github.com>
2026-09-07 22:26:46 +0300

    Шаг 6. Создал клавиатуру

--- app/src/main/java/com/example/calculatoruipractice/MainActivity.kt
+++ app/src/main/java/com/example/calculatoruipractice/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -50,6 +51,12 @@ fun CalculatorV1() {
                 .padding(bottom = 16.dp),
             maxLines = 1
         )
+
+        Column(
+            modifier = Modifier.weight(3f),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+        }
     }
 }
 
