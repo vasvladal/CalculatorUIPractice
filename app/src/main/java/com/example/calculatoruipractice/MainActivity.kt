5096d385204c754785911bc0e3668fbcca048508
vasvladal <vasvladal@users.noreply.github.com>
2026-09-07 22:18:08 +0300

    Шаг 4. Создал главный Column

--- app/src/main/java/com/example/calculatoruipractice/MainActivity.kt
+++ app/src/main/java/com/example/calculatoruipractice/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.calculatoruipractice.ui.theme.CalculatorUIPracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,5 +32,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CalculatorV1() {
     // интерфейс калькулятора
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+    }
 }
 
