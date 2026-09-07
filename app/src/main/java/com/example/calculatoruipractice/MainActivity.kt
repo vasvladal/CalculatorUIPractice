e3926849274ec3a3019dd322a1685696c2762186
vasvladal <vasvladal@users.noreply.github.com>
2026-09-07 22:22:26 +0300

    Шаг 5. Создал дисплей

--- app/src/main/java/com/example/calculatoruipractice/MainActivity.kt
+++ app/src/main/java/com/example/calculatoruipractice/MainActivity.kt
@@ -6,13 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.calculatoruipractice.ui.theme.CalculatorUIPracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,6 +40,16 @@ fun CalculatorV1() {
             .fillMaxSize()
             .padding(16.dp)
     ) {
+        Text(
+            text = "0",
+            fontSize = 64.sp,
+            textAlign = TextAlign.End,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(2f)
+                .padding(bottom = 16.dp),
+            maxLines = 1
+        )
     }
 }
 
